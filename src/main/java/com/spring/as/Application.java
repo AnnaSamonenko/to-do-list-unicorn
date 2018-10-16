10992329a0feebb2961781d383bc450f34399ab7
Anna Samonenko <annsamonenko97@gmail.com>
2018-10-17 00:04:25 +0300

    Added script for add task

--- src/main/java/com/spring/as/Application.java
+++ src/main/java/com/spring/as/Application.java
@@ -1,14 +1,9 @@
 package com.spring.as;
 
-import com.spring.as.dao.TaskDAO;
-import com.spring.as.dao.TaskDAOImpl;
-import com.spring.as.entity.TaskDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.time.LocalDate;
 
 @ComponentScan
 @SpringBootApplication
@@ -16,13 +11,6 @@ import java.time.LocalDate;
 public class Application {
 
     public static void main(String[] args) {
-        TaskDTO t = new TaskDTO();
-        t.setTitle("Learn Hibernate");
-        t.setDescription("Be best");
-        t.setDate(LocalDate.now());
-        TaskDAO taskDAO = new TaskDAOImpl();
-        taskDAO.create(t);
-
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/resources/static/js/task.js
+++ src/main/resources/static/js/task.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //print all data from db
     $.ajax({
         url: "http://localhost:8080/rest/task/all"
     }).then(function (data) {
@@ -12,4 +13,17 @@ $(document).ready(function () {
         });
         $('.task_table').append(task_data);
     });
+
+    // add data to db
+    $("button.add_task").click(function () {
+        var form = $("<form></form>");
+        form.append('Title: <input type="text" name="title"><br>' +
+            'Description: <input type="text" name="description"><br>' +
+            '<input type="button" value="close">' +
+            '<input type="submit">');
+        $("table").after().append(form);
+    });
 });
+
+
+
