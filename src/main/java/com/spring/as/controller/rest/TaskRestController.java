a805f7cfc10d79cfb25a08d41455c57ada05f7ee
Anna Samonenko <annsamonenko97@gmail.com>
2018-10-16 23:14:17 +0300

    Added service for adding new task

--- src/main/java/com/spring/as/controller/rest/TaskRestController.java
+++ src/main/java/com/spring/as/controller/rest/TaskRestController.java
@@ -5,7 +5,6 @@ import com.spring.as.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
