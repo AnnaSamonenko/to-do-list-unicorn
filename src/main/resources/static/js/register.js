4a168f534dc1b34125a40d55fdca6968ec9a4506
AnnaSamonenko <annsamonenko97@gmail.com>
2018-10-18 13:08:27 +0300

    Fix script for registration

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,21 +1,26 @@
 $(document).ready(function () {
- $("input.register").click(function () {
-
-    var loginValue = $("input.login").val();
-    var emailValue = $("input.email").val();
-    var passwordValue = $("input.password").val();
-    $.ajax({
-    type : "POST",
-    url :"http://localhost:8080/rest/user/add",
-    data : {login:loginValue, email: emailValue, password: passwordValue},
-    timeout : 100000,
-    success : function(response) {
-    alert(response);
-     },
-    error : function(e) {
-    console.log("ERROR: ", e);
+    $("input.register").click(function () {
+        var loginValue = $("input.login").val();
+        var emailValue = $("input.email").val();
+        var passwordValue = $("input.password").val();
+        $.ajax({
+        type : "POST",
+        contentType: "application/json; charset=utf-8",
+        url :"http://localhost:8080/rest/user/add",
+        data: JSON.stringify({
+            login: $("input.login").val(),
+            email: $("input.email").val(),
+            password: $("input.password").val()
+        }),
+        timeout : 100000,
+        success : function(response) {
+        alert(response);
+         },
+        error : function(e) {
+        var data =
+        alert("ERROR: ", e);
+        alert($("input.login").val() + " " + $("input.email").val() + " "+ $("input.password").val());
+        }
+        });
     });
-    }
- });
-});
 });
