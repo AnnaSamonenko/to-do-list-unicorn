6a297d0db2cede35d9d05262e2a0f371be81e3a3
AnnaSamonenko <annsamonenko97@gmail.com>
2018-10-22 17:55:46 +0300

    Config UserService and UserDetails

--- src/main/java/com/spring/as/entity/User.java
+++ src/main/java/com/spring/as/entity/User.java
@@ -2,15 +2,19 @@ package com.spring.as.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Getter
     @Setter
@@ -22,7 +26,7 @@ public class User {
 
     @Getter
     @Setter
-    @Column(name = "login")
+    @Column(name = "username", unique = true)
     private String login;
 
     @Getter
@@ -35,8 +39,42 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
     @Getter
     @Setter
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
     private List<Project> projects = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
