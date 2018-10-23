5102218ee9df7f5bfe56c7b81a06c0d71ebaced4
AnnaSamonenko <annsamonenko97@gmail.com>
2018-10-23 10:54:07 +0300

    Done with login

--- src/main/java/com/spring/as/security/SecurityConfig.java
+++ src/main/java/com/spring/as/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.spring.as.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -31,5 +33,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
         http.csrf().disable();
     }
-    
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
 }
