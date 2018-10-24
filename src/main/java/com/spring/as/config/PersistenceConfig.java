157cc4ac104f430f07d8e96076ef7e64a1685882
AnnaSamonenko <annsamonenko97@gmail.com>
2018-10-24 16:16:52 +0300

    Clean imports

--- src/main/java/com/spring/as/config/PersistenceConfig.java
+++ src/main/java/com/spring/as/config/PersistenceConfig.java
@@ -2,16 +2,12 @@ package com.spring.as.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
