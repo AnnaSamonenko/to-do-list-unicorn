41c7071ca0b9d26c4d9ee92d626ba7551da5e723
AnnaSamonenko <annsamonenko97@gmail.com>
2018-11-05 17:25:45 +0200

    Change hibernate dialect according to database version

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         $.ajax({
             type: "POST",
             contentType: "application/json; charset=utf-8",
-            url: "http://localhost:8080/api/user/add",
+            url: "http://localhost:8080/api/user/registration",
             data: JSON.stringify({
                 username: $("#name").val(),
                 email: $("#email").val(),
