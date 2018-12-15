20b47b265d982fb5528f9a6e83258e40a60fc54d
Anna Samonenko <annsamonenko97@gmail.com>
2018-12-15 07:16:30 +0200

    Replace buttons

--- src/main/java/com/spring/as/controller/MainController.java
+++ src/main/java/com/spring/as/controller/MainController.java
@@ -2,12 +2,13 @@ package com.spring.as.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/index")
-    String index() {
+    String index(@RequestParam("project_id") String projectId) {
         return "index";
     }
 
